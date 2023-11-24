c5568e6b2b116c1b1ea3dd99cea40994227826c7
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-24 14:08:14 +0100

    feat: Basic Installation done. BE: App, index, router and controller. build for TSC

---
+++ server/build/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const router_1 = __importDefault(require("./router"));
+const cors = require("cors");
+app.use(cors());
+app.use(express_1.default.json());
+app.use(router_1.default);
+app.get('/ping', (_req, res) => {
+    console.log('aaa');
+    res.send('pong');
+});
+exports.default = app;
---
+++ server/router.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+//import { test } from './controllers/testController';
+
+const router = Router();
+
+//router.post("/login", test);
+
+
+
+export default router
