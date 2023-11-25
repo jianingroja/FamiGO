548d75ac5417970e2dd3a5036128df8521a37efc
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-25 15:12:57 +0100

    feat: isAuthenticated middleware

---
+++ server/middlewares/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../models/users';
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    // extract cookie
+    const sessionToken = req.cookies['TEST'];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+};
