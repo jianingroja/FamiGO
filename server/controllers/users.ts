0e4ffa67d0259d87146a2b9a0e53382a3d0d29af
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-25 14:53:35 +0100

    feat: login function done

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -2,9 +2,58 @@ import express from 'express';
 import { createUser, getUserByEmail, getUserByUserName } from '../models/users';
 import { random, authentication } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+
+    if (!user || !user.authentication) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = user.authentication.salt
+      ? authentication(user.authentication.salt, password)
+      : null;
+
+    // if (!user) {
+    //   return res.sendStatus(400);
+    // }
+
+    // const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie('TEST', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
-    const { email, password, username, avatar } = req.body;
+    const { email, password, username } = req.body;
 
     if (!email || !password || !username) {
       return res.sendStatus(400);
@@ -25,7 +74,6 @@ export const register = async (req: express.Request, res: express.Response) => {
         salt,
         password: authentication(salt, password),
       },
-      avatar,
     });
 
     return res.status(200).json(user).end();
--- server/router.ts
+++ server/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { register } from './controllers/users';
+import { register, login } from './controllers/users';
 
 const router = Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
