29564acb6d642875ad5a1dab6b42e01c88e00c15
wangjianing <jianingmica@gmail.com>
2023-12-04 13:23:50 +0100

    feat: toast related to publish

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 import useAuth from './hooks/useAuth';
 
@@ -28,6 +29,7 @@ const App = () => {
 
   return (
     <div className="app">
+      <Toaster position="top-center" />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
