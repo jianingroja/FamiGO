46da86955ccbc148152bc00881a7a43cd0d9ee2a
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-12-04 13:32:23 +0100

    refactor: naming ActivityPage

---
+++ client/src/pages/Activity/ActivityPage.tsx
@@ -0,0 +1,11 @@
+import SpecificActivityCopy from '../../components/Activity/SpecificActivityCopy.tsx';
+import './ActivityPage.css';
+
+const Activity = () => {
+  return (
+    <div className="activity-page">
+      <SpecificActivityCopy />
+    </div>
+  );
+};
+export default Activity;
