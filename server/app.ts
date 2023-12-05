2745bf85af9bb16e10efe50ae9b45058831b2876
wangjianing <jianingmica@gmail.com>
2023-12-05 15:32:24 +0100

    cookie setting

--- server/app.ts
+++ server/app.ts
@@ -4,9 +4,11 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:5173';
 
 app.use(
   cors({
+    origin: corsOrigin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
