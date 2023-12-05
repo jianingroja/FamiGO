6e4c983df6f2374b68797fea98e639ec08fe64aa
wangjianing <jianingmica@gmail.com>
2023-12-05 11:59:58 +0100

    edit cors config

--- server/app.ts
+++ server/app.ts
@@ -7,18 +7,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
 app.use(express.json());
 app.use(cookieParser());
-
-// app.use((req, _res, next) => {
-//   console.log(`Received request: ${req.method} ${req.path}`);
-//   next();
-// });
 
 app.use(router);
 
